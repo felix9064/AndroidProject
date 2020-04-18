d41cff558d0284e3872bd075288bac60b4a4ec59
zhangzhh <zhang1zhi7hua5@163.com>
2020-04-18 10:46:28 +0800

    安卓项目

---
+++ MyFirstApp/app/src/main/java/com/felix/myfirstapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.felix.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.felix.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** 在用户点击发送按钮时调用 */
+    public void sendMessage (View view) {
+        // 对按钮做出一些响应
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
